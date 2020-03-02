94709024e1e895374675384746280ccd878f35f3
DariaKaduchka <Daria.Kaduchka@gmail.com>
2020-03-02 22:06:41 +0100

    fixed linter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
-const greeting = () => {
-const userName = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + userName + '!');
-}
 
-export {greeting};
+const greeting = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
+
+export default greeting;
